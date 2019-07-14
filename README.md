b669d27ca3515ca4ea7e122c1cd9b751d6916ed9
venomz75 <vc75@me.com>
2019-07-14 20:14:57 +0100

    Changes: boards now chosen via their tag(e.g. /wg/) instead of an index number. Code has also been optimised and tidied a bit.

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
-#### papeScrape is a basic Python webscraper which downloads all the images in a 4chan thread. As the name may suggest; the initial intention was to download wallpapers from it's [/wg/](http://boards.4chan.org/wg/catalog) board.
+#### papeScrape is a basic Python webscraper which downloads all of the images in a 4chan thread. As the name may suggest; the initial intention was to download wallpapers from it's [/wg/](http://boards.4chan.org/wg/catalog) board.
 
 ## Current features:
-* NSFW boards are opt-in
-* Selection of boards
-* Selection of threads within chosen board
-* Download all images in a thread, one thread at a time
-* Saves OP message in an op.txt file
+* NSFW boards are included or excluded depending on the user's choice
+* Can choose any thread from any board
+* All image, .webm, .pdf etc. files from a thread saved in it's own directory
+* Saves OP's message in a .txt file in that same directory
 
 ## Planned changes:
-* Add selection of boards by tag or name (wg or Wallpapers/General).
 * Add selection of threads by post number or topic.
 * Add an image count to the threads.
 * Add capability for multiple concurrent thread downloads.
--- papeScrape.py
+++ papeScrape.py
@@ -8,65 +8,61 @@ import json
 import re
 import os
 
-boardTotal = []
-boardLinks = []
-boardNames = []
-boardTags = []
-threadLinks = []
-threadSubjects = []
-threadNumbers = []
+boardList = [] #[wg, "Wallpapers/General", "URL"]
+threadList = [] #[534341, "Space papes", "URL"]
+
+def linkToSoup(url):
+    data = requests.get(url)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    return soup
 
 def nsfwFilter():
-    nsfwSelection = distutils.util.strtobool(input("\nShow NSFW boards? (y/n)"))
+    nsfwSelection = distutils.util.strtobool(input("\nShow NSFW boards? (y/n): ")); print(" ")
     return nsfwSelection
 
-def scrapeBoards(nsfwSelection):
-    data = requests.get("http://www.4chan.org/")
-    soup = BeautifulSoup(data.text, 'html.parser')
 
+def scrapeBoards(nsfwSelection):
+    soup = linkToSoup("http://www.4chan.org/")
+    boardLinks = []; boardNames = []; boardTags = []
+    
     for a in soup.find_all("a", {"class": "boardlink", "href": True}):
         if len(a["href"]) < 30:
             boardLinks.append(a["href"])
-            tag = a["href"][7:]
-            boardIndex = tag.index("/")
-            tag = tag[boardIndex+1:]
-            boardIndex = tag.index("/")
-            tag = tag[0:boardIndex]
+            slash1 = a["href"].index("/", 2) + 1; slash2 = a["href"].rindex("/")
+            tag = a["href"][slash1:slash2] 
             boardTags.append(tag)
         if a.text:
             boardNames.append(a.text)
 
     for i in range(len(boardLinks)):
-        boardTotal.append([boardTags[i],boardNames[i],boardLinks[i]])
-        boardTotal.sort(key=lambda x:x[0])
+        boardList.append([boardTags[i],boardNames[i],boardLinks[i]])
+        boardList.sort(key=lambda x:x[0])
 
     if not nsfwSelection:
-        for i in reversed(range(len(boardTotal))):
-            if "4channel" not in boardTotal[i][2]:
-                del(boardTotal[i])
+        for i in reversed(range(len(boardList))):
+            if "4channel" not in boardList[i][2]:
+                del(boardList[i])
 
-    for i in range(len(boardTotal)):
-        print(str(i).zfill(2)+" /"+boardTotal[i][0]+"/ "+(" "*(4-len(boardTotal[i][0])))+boardTotal[i][1])
-    boardSelection = input("\nChoose your board: ")
-    boardSelection = int(boardSelection)
-    return boardSelection 
+    for i in range(len(boardList)):
+        print("/"+boardList[i][0]+"/ "+(" "*(4-len(boardList[i][0])))+boardList[i][1])
+    tagSelection = input("\nChoose your board: ")
+
+    i=0
+    while boardList[i][0] != tagSelection:
+        i += 1
+    print("\nParsing /"+boardList[i][0]+"/, please wait...\n") 
+    boardSelection = i    
+    return int(boardSelection)
+    
 
 def scrapeThreads(boardSelection):
-    try:
-        boardSelection = int(boardSelection)
-    except ValueError:
-        print("\nInvalid input!")
-    else:
-        if boardSelection > 69 or boardSelection < 0:
-            print("\nInvalid input!")
-        else:
-            print("\nParsing /"+boardTags[boardSelection]+"/, please wait...\n")    
-
+    threadLinks = []; threadSubjects = []; threadNumbers = []
+    
     for i in range(11):
-        data = requests.get("http:"+boardLinks[boardSelection]+str(i))
-        soup = BeautifulSoup(data.text, 'html.parser')
+        soup = linkToSoup("http:"+boardList[boardSelection][2]+str(i))
         for div in soup.find_all("div", {"class": "thread"}):
             threadNumbers.append(div["id"][1:])
+            threadLinks.append("http:"+boardList[boardSelection][2]+"thread/"+div["id"][1:])
         for div in soup.find_all("div", {"class": "postInfo desktop"}):
             for span in div.find_all("span", {"class": "subject"}):
                 if not span.text:
@@ -74,14 +70,15 @@ def scrapeThreads(boardSelection):
                 else:
                     threadSubjects.append(span.text)
 
-    for i in range(len(threadSubjects)):
-        threadLinks.append("http:"+boardLinks[boardSelection]+"thread/"+threadNumbers[i])
-        print (str(i)+") "+threadNumbers[i]+" - "+threadSubjects[i])
+    for i in range(len(threadLinks)):
+        threadList.append([threadNumbers[i],threadSubjects[i],threadLinks[i]])
+        threadList.sort(key=lambda x:x[0])
+
+    for i in range(len(threadList)):
+        print (str(i).zfill(3)+") "+threadList[i][0]+" - "+threadList[i][1])
 
     threadSelection = input("\nChoose your thread: ")
-    threadSelection = int(threadSelection)
-    return threadSelection
-
+    return int(threadSelection)
 
 
 def scrapeImages(boardSelection, threadSelection):
@@ -93,41 +90,31 @@ def scrapeImages(boardSelection, threadSelection):
         if threadSelection > 149 or threadSelection < 0:
             print("\nInvalid input!")
         else:
-            print("\nDownloading images from thread #"+threadNumbers[threadSelection]+" on /"+boardTags[boardSelection]+"/, please wait...")
+            print("\nDownloading images from thread #"+threadList[threadSelection][0]+" on /"+boardList[boardSelection][0]+"/, please wait...")
     
-    data = requests.get(threadLinks[threadSelection])
-    soup = BeautifulSoup(data.text, 'html.parser')
-    filepath = os.getcwd()
+    soup = linkToSoup(threadList[threadSelection][2])
 
-    if threadSubjects[threadSelection] == "No subject":
-        threadSubjects[threadSelection] = threadSubjects[threadSelection]+threadNumbers[threadSelection]
-    threadSubjects[threadSelection] = re.sub('[^A-Za-z0-9]+', '', threadSubjects[threadSelection])
-    newdir = filepath+"/"+boardTags[boardSelection]+"/"+threadSubjects[threadSelection]+"/"
+    threadList[threadSelection][1] += threadList[threadSelection][0] if threadList[threadSelection][1] == "No subject" else threadList[threadSelection][1]
+    newdir = os.getcwd()+"/"+boardList[boardSelection][0]+"/"+re.sub('[^A-Za-z0-9]+', '', threadList[threadSelection][1])+"/"
 
     try:
-        if os.path.isdir(filepath+"/"+boardTags[boardSelection]):
+        if os.path.isdir(os.getcwd()+"/"+boardList[boardSelection][0]):
             os.mkdir(newdir)  
         else:
-            os.mkdir(boardTags[boardSelection])
+            os.mkdir(boardList[boardSelection][0])
             os.mkdir(newdir)
     except OSError:
         print("\nFailed to create directory "+newdir)
         print("\nExiting papeScrape...\n")
     else:
         print("\nCreated directory "+newdir+" successfully")
-        opText = soup.find("blockquote", {"class": "postMessage"}).text
-        textFile = open(newdir+"op.txt", "w")
-        textFile.write(opText)
-        textFile.close()
+        textFile = open(newdir+"op.txt", "w"); textFile.write(soup.find("blockquote", {"class": "postMessage"}).text); textFile.close()  
         print("\nSaved OP message as "+newdir+"op.txt\n")
 
-        results = []
         for a in soup.find_all("a", {"class": "fileThumb", "href": True}):
-            results.append(a["href"])
+            urllib.request.urlretrieve("http:"+a["href"], newdir+a["href"][19:])
+            print("Downloaded "+a["href"][19:])
 
-        for i in results:
-            urllib.request.urlretrieve("http:"+i, newdir+i[19:])
-            print("Downloaded "+i[19:])
         print("\nAll images downloaded.")
         print("\nExiting papeScrape...\n")
 
